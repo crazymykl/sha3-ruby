f0cc6856e12b9baa606d230f38a037ad18faa804
Mike MacDonald (crazymykl) <crazymykl@gmail.com>
2012-10-08 19:01:22 -0400

    all functions implemented

--- ext/sha3/sha3.c
+++ ext/sha3/sha3.c
@@ -4,28 +4,92 @@
 static VALUE mSha3;
 static VALUE cSha3Engine;
 
-static VALUE sha3_init(VALUE self, VALUE bitlength) {
+typedef struct {
+  unsigned int hashbits;
+  hashState state;
+} SHAobject;
+
+static VALUE sha3_init(VALUE class, VALUE bitlength) {
   
+  SHAobject *o;
   VALUE state;
-  hashState *hs;
   
-  hs = malloc(sizeof(hashState));
-  Init(hs, INT2FIX(bitlength));
-  state = Data_Make_Struct(self, hashState, 0, free, hs);
-  free(hs);
+  o = malloc(sizeof(SHAobject));
+  o->hashbits = FIX2INT(bitlength);
   
+  Init(&o->state, o->hashbits);
+
+  state = Data_Wrap_Struct(cSha3Engine, 0, free, o);
+  rb_obj_call_init(state, 0, NULL);
   return state;
 }
 
-static VALUE sha3_update(VALUE self, VALUE key, VALUE setting) {
-  printf("boo!\n");
-  return Qnil;
+static VALUE sha3_update(VALUE self, VALUE str) {
+  
+  SHAobject *o;
+  BitSequence *data;
+  unsigned int data_length = RSTRING_LEN(str);
+  
+  data = StringValuePtr(str);
+  
+  Data_Get_Struct(self, SHAobject, o);
+  
+  Update(&o->state, data, data_length*8);
+  
+  return self;
+}
+
+static VALUE sha3_final(VALUE self) {
+  
+  SHAobject *o;
+  VALUE out;
+  hashState *hs;
+  BitSequence *hash_val;
+  unsigned int hash_len;
+    
+  Data_Get_Struct(self, SHAobject, o);
+
+  hash_len = o->hashbits;
+  hash_val = malloc(hash_len*sizeof(BitSequence)/8);
+  hs = malloc(sizeof(hashState));
+  
+  memcpy(hs, &o->state, sizeof(hashState));
+    
+  Final(hs, hash_val);
+  
+  out = rb_str_new(hash_val, hash_len/8);
+  
+  free(hash_val);
+  free(hs);
+  
+  return out;
+  
+}
+
+static VALUE sha3_hash(VALUE self, VALUE str, VALUE hashbits) {
+  
+  VALUE out;
+  BitSequence *hash_val, *data;
+  unsigned int data_length = RSTRING_LEN(str), hash_len = FIX2INT(hashbits);
+  
+  data = StringValuePtr(str);
+  
+  hash_val = malloc(hash_len*sizeof(BitSequence)/8);
+  Hash(hash_len, data, data_length*8, hash_val);
+  
+  out = rb_str_new(hash_val, hash_len/8);
+  
+  free(hash_val);
+  
+  return out;
 }
 
 void Init_sha3(){
   mSha3 = rb_define_module("Sha3");
-  cSha3Engine = rb_define_class_under(mSha3, "Engine", rb_cObject);
+  cSha3Engine = rb_define_class_under(mSha3, "Instance", rb_cObject);
+  rb_define_singleton_method(mSha3, "__hash", sha3_hash, 2);
 
-  rb_define_singleton_method(cSha3Engine, "__init", sha3_init, 2);
-  rb_define_singleton_method(cSha3Engine, "__update", sha3_update, 2);
+  rb_define_singleton_method(cSha3Engine, "__init", sha3_init, 1);
+  rb_define_method(cSha3Engine, "__update", sha3_update, 1);
+  rb_define_method(cSha3Engine, "__final", sha3_final, 0);
 }
--- lib/sha3-ruby.rb
+++ lib/sha3-ruby.rb
@@ -2,6 +2,26 @@ require "sha3"
 require "sha3-ruby/version"
 
 module Sha3
-  class Engine
+  class Instance
+        
+    def update str
+      __update str
+    end
+    
+    def digest
+      __final
+    end
+    
+    alias :'<<' :update 
+
   end
+    
+  def self.new bits=224
+    Instance.__init(bits)
+  end
+  
+  def self.digest str, bits=224
+    __hash(str, bits)
+  end
+
 end
